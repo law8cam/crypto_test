345d86066975e3597086418416ed3c214359a626
law8cam <lawrencecameron123@hotmail.com>
2025-01-14 11:46:39 +0000

    Update

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { Wallet } from 'ethers';
 
 function createNewEthereumAddress() {
 
-    // var address1 = "0x19833badEf48966AC870c5A1e05718E929bf8891"
-    // var example = "0x9f04b53c21752922cf30a98db5b9bf7be522a9df5c6c5dd2c9fb0d4043dfde78"
+    var address1 = "0x19833badEf48966AC870c5A1e05718E929bf8891"
+    var example = "0x9f04b53c21752922cf30a98db5b9bf7be522a9df5c6c5dd2c9fb0d4043dfde78"
 
     // Generate a new random wallet
     const wallet = Wallet.createRandom();
@@ -16,7 +16,7 @@ function createNewEthereumAddress() {
     console.log("Ethereum Address:", address);
     console.log("Private Key:", privateKey);
 
-    "cube seek space unique renew cable one stumble ring video artefact hammer"
+    var seed = "cube seek space unique renew cable one stumble ring video artefact hammer"
 
     'unique point space unique renew cable one stumble ring video artefact hammer'
 
